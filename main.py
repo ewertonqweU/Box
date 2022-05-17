0001996f78e687425a49e8a8004efac2814b92b6
ewertonqweU <ejesantos.outlook@gmail.com>
2022-05-17 10:13:40 -0300

    Adicionando reconhecedor
    Adicionando reconhecedor do google com tratamento de ruido e tratamento de erro

---
+++ main.py
@@ -0,0 +1,16 @@
+from email.mime import audio
+import speech_recognition as sr
+
+# criar reconhecedor
+r = sr.Recognizer()
+
+# abrir mic para captura
+with sr.Microphone() as source:
+    r.adjust_for_ambient_noise(source)
+    print('Pode falar que eu vou gravar')
+    while True:
+        audio = r.listen(source)
+        try:
+            print(r.recognize_google(audio, language="pt-BR"))
+        except:
+            print('desculpe nao entendi')
